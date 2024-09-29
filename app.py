32c7122636fb1d9aab7435ca781687089a851447
achraf-el-auto-1 <achraf.elkasri03@gmail.com>
2024-09-29 20:04:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hahahahahvvvvvvvvvvvvvvvvvvvvvvvv!'
+@app.route('/your_endpoint', methods=['POST'])
+def handle_post():
+    data = request.get_json()
+    variable1 = data.get('variable1')
+    variable2 = data.get('variable2')
+    # Process the variables as needed
+    return jsonify({'message': 'Variables received successfully'})
 
 if __name__ == '__main__':
     app.run()
